18c9721ef5beb10172c9ffa7162ffed7834da600
gtagua <31631740+gtagua@users.noreply.github.com>
2017-09-05 14:06:31 -0300

    Arquivo Principal para teste

---
+++ main.py
@@ -0,0 +1,15 @@
+import pycom
+import time
+pycom.heartbeat(False)
+for cycles in range(30): # stop after 10 cycles
+    pycom.rgbled(0x007f00) # green
+    time.sleep(10)
+    pycom.rgbled(0x7f7f00) # yellow
+    time.sleep(3)
+    pycom.rgbled(0x7f0000) # red
+    time.sleep(15)
+     print("hello WiPy!")
+
+
+
+pycom.rgbled(0x000000) # black
